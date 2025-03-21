9309c5486a3667ba14f1f4cc89d255f8b5709f84
box3101 <box4101@nate.com>
2025-03-21 15:07:55 +0900

    Refactor sidebar component to use dynamic menu items

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -3,15 +3,15 @@
     <div class="sidebar-container">
       <!-- 메뉴 아이템들 -->
       <ul class="menu-list">
-        <li class="menu-item">
-          <NuxtLink to="/" class="menu-link">
+        <li class="menu-item" v-for="menu in menus" :key="menu.id">
+          <NuxtLink :to="menu.link" class="menu-link">
             <div class="menu-icon">
-              <Icon name="uil:dashboard" size="24" />
+              <Icon :name="menu.icon" size="24" />
             </div>
-            <div class="menu-text">대시보드</div>
+            <div class="menu-text">{{menu.text}}</div>
           </NuxtLink>
         </li>
-        <li class="menu-item">
+        <!-- <li class="menu-item">
           <NuxtLink to="/cnt01" class="menu-link">
             <div class="menu-icon">
               <Icon name="uil:clipboard-notes" size="24" />
@@ -26,11 +26,42 @@
             </div>
             <div class="menu-text">PMS</div>
           </NuxtLink>
-        </li>
+        </li> -->
       </ul>
     </div>
   </aside>
 </template>
+
+<script setup>
+
+ const menus = [
+  {
+    id: 1,
+    link: '/',
+    icon: 'uil:dashboard',
+    text: '대시보드'
+  },
+  {
+    id: 2,
+    link: '/cnt01',
+    icon: 'uil:clipboard-notes',
+    text: '업무1'
+  },
+  {
+    id: 3,
+    link: '/cnt02',
+    icon: 'uil:clipboard-notes',
+    text: '업무2'
+  },
+  {
+    id: 4,
+    link: '/cnt03',
+    icon: 'uil:clipboard-notes',
+    text: '업무3'
+  },
+ ]
+
+</script>
 
 <style scoped>
 </style>
