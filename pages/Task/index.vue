cd8ae52245ea5aff898b7cec566eceec96e9ae7c
box3101 <box4101@nate.com>
2025-03-25 08:50:51 +0900

    Task - task 수정

--- pages/Task/index.vue
+++ pages/Task/index.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script setup>
-import TasksBoard from '~/pages/Task/comps/TaskBoard.vue';
 import { inject } from 'vue';
 
-// 로고 텍스트 접근
+// 로고 텍스트 접근 
 const logoText = inject('logoText');
 
 // 원하는 시점에 로고 텍스트 변경
