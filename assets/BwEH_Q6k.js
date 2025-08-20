f6ed42a8000fb00df90cc60abb2ab8319b83cb40
box3101 <box4101@nate.com>
2025-08-20 06:21:02 +0000

    Deploying to gh-pages from @ box3101/ispark-sgate@be4265ab08a3da9818c9c506729fa27d14704ee0 🚀

---
+++ assets/BwEH_Q6k.js
@@ -0,0 +1,1 @@
+import{c as n,o}from"./DBQ0s5Ec.js";const m={__name:"Emit",emits:["btnClick"],setup(a,{emit:t}){const c=t,e=()=>{c("btnClick","abc")};return(i,s)=>(o(),n("button",{onClick:e},"클릭"))}};export{m as default};
